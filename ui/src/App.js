a8c0bd20cb3dffd51f4d8132ceecf3f0c877a83a
Chris Chase <chris@chasenc.com>
2018-04-23 23:07:54 -0400

    Added patternfly-react

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { VerticalNav } from 'patternfly-react';
 
 import { Routes } from './Routes'
+
+import '../node_modules/patternfly/dist/css/patternfly.css';
+import '../node_modules/patternfly/dist/css/patternfly-additions.css';
+import '../node_modules/patternfly-react/dist/css/patternfly-react.css';
 import './App.css';
 
 class App extends Component {
+  handleNavClick = navItem => {
+    switch (navItem.title) {
+      case 'Home':
+        this.props.history.push('/');
+        break;
+      case 'Route 2':
+        this.props.history.push('/route2');
+        break;
+      case 'Route 3':
+        this.props.history.push('/route3');
+        break;
+      default:
+        console.log('Unrecognized navbar item clicked');
+    }
+  };
+
+
   render() {
     return (
-      <div className="App container">
-        <main className="main-content">
-          <Routes/>
-        </main>
+      <div className="App">
+        <VerticalNav>
+          <VerticalNav.Masthead title="Demo App" />
+          <VerticalNav.Item
+            title="Home"
+            iconClass="fa fa-dashboard"
+            onClick={this.handleNavClick}
+            className={null}
+          />
+          <VerticalNav.Item
+            title="Route 2"
+            iconClass="fa fa-star"
+            onClick={this.handleNavClick}
+            className={null}
+          />
+        </VerticalNav>
+        <Routes/>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
--- ui/src/pages/Home.js
+++ ui/src/pages/Home.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Graph from "../components/Graph";
 
 const Home = () => (
-  <div className="Home">
+  <div className="Home container-fluid container-pf-nav-pf-vertical">
     <Graph />
   </div>
 );
